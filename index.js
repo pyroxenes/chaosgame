1b5031ed7a1b33f9f1fcdd81d3dc85d38008e53e
jade <145362301+pyroxenes@users.noreply.github.com>
2024-08-08 18:11:49 -0700

    site functional now :3

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 // JavaScript
 
 const settings = {
-    n: 0,
-    itter: 0,
+    n: 3,
+    itter: 5000,
     point: [],
+    ratio: .5,
 }
 
 const settingscontent = document.getElementsByClassName("settingscontent")[0];
 const settingsbutton = document.getElementsByClassName("settingsbutton")[0];
 settingsbutton.onclick = () => {
-    settingscontent.classList.toggle("show")
+    settingscontent.classList.toggle("show");
 }
 
     /** @type {HTMLCanvasElement} */
-    const canvas=document.getElementById("canvas");
+    const canvas = document.getElementById("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    const ctx=canvas.getContext("2d");
-    const halfcanvheight=canvas.height * .5;
-    const halfcanvwidth=canvas.width * .5;
+    const ctx = canvas.getContext("2d");
+    const halfcanvheight = canvas.height * .5;
+    const halfcanvwidth = canvas.width * .5;
     const r = canvas.width < canvas.height ? (canvas.width / 8 )* 3 : (canvas.height / 8) * 3;
+
+    const ratioin = document.getElementById("ratios");
+    const ratioout = document.getElementById("ratiosout")
+    ratioout.innerHTML = ratioin.value;
+    ratioin.oninput = function() {
+        ratioout.innerHTML = this.value
+        settings.ratio = this.value
+    }
 
     const vertexcount = document.getElementById("vertices");
     const vertexout = document.getElementById("verticesout");
@@ -27,10 +36,10 @@ settingsbutton.onclick = () => {
     vertexcount.oninput = function() {
         vertexout.innerHTML = this.value;
         settings.n = this.value;
-        const circledivs = (2 * Math.PI) / settings.n
+        const circledivs = (2 * Math.PI) / settings.n;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        settings.point=[];
-        const ninty = Math.PI / 2
+        settings.point = [];
+        const ninty = Math.PI / 2;
         for (let i = 0; i<settings.n; i++) {
             xVal = r * Math.cos((i+1)*circledivs - ninty);
             yVal = r * Math.sin((i+1)*circledivs - ninty);
@@ -51,9 +60,33 @@ settingsbutton.onclick = () => {
 
     const run = document.getElementsByClassName("run")[0];
     run.onclick = () => {
-        ctx.fillStyle = "#3dbbdb";
-        ctx.fillRect(halfcanvwidth, halfcanvheight, 1, 1);
+        for (let i = 0; i<settings.itter; i++) {
+            if (i === 0) {
+                const randnumber = Math.floor(Math.random()*settings.n);
+                const randnumber2 = Math.floor(Math.random()*(settings.n-randnumber));
+                console.log(randnumber)
+                console.log(randnumber2)
+                const xVal = ((settings.point[randnumber].x-settings.point[randnumber2].x)*settings.ratio) + settings.point[randnumber2].x;
+                const yVal = ((settings.point[randnumber].y-settings.point[randnumber2].y)*settings.ratio) + settings.point[randnumber2].y;
+                settings.point.push({x: xVal,y: yVal});
+            } else {
+                const randnumber = Math.floor(Math.random()*settings.n);
+                const xVal = (settings.point[randnumber].x-settings.point[settings.point.length-1].x)*settings.ratio + settings.point[settings.point.length-1].x;
+                const yVal = (settings.point[randnumber].y-settings.point[settings.point.length-1].y)*settings.ratio + settings.point[settings.point.length-1].y;
+                settings.point.push({x: xVal,y: yVal});
+            }
+            // console.log(settings.point)
+            ctx.fillStyle = "#3dbbdb";
+            ctx.fillRect(settings.point[settings.point.length-1].x + halfcanvwidth, settings.point[settings.point.length-1].y + halfcanvheight, 1, 1);
+        }
     }
 
+
+
+
+/*   _.,      _.._
+    (.-.`._,'(,-'""`|\`-/|
+        `-.-, \ )-' (=^w^=)
+  pyroxenes  `- -'  \_' '_/  */
 
 
